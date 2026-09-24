de494f5d3fa345d0f18e6609df16f2cfc04d5569
vasilii2008 <vasilii.2008.spb@gmail.com>
2026-09-24 11:20:12 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ex17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            char lletra = rand.Next(65-91);
+            Console.WriteLine($"{(string)lletra}");
+
+    }
+}
